eec3133173ec52deb577dc99d04e2d5917b8af2c
Rafael Dallaqyan <rafaeldallakyan4@gmail.com>
2024-11-20 01:12:22 +0400

    changed

--- src/pages/basket/basketProduct/BasketProduct.tsx
+++ src/pages/basket/basketProduct/BasketProduct.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../basketStyles.scss";
 
 export default function BasketProduct({
@@ -9,9 +10,30 @@ export default function BasketProduct({
     count: number | string;
   };
 }) {
+  const [count, setCount] = useState(+product.count || 0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClickIncrement = (e: any) => {
+    e.stopPropagation();
+    setIsLoading(true);
+    setTimeout(() => {
+      setCount((prev) => prev + 1);
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  const handleClickDecrement = (e: any) => {
+    e.stopPropagation();
+    setIsLoading(true);
+    setTimeout(() => {
+      setCount((prev) => prev - 1);
+      setIsLoading(false);
+    }, 1000);
+  };
+
   return (
     <div className="basket-product__container">
-      <div>
+      <div className="image_container">
         <img
           src="https://samsa.ucoz.ae/_sh/00/24b.jpg?v=468"
           width={32}
@@ -21,14 +43,34 @@ export default function BasketProduct({
       <div className="body">
         <h3>{product.title}</h3>
         <p>{product.price}</p>
-        <div className="btn__container">
-          <button>-</button>
-          <p>{product.count}</p>
-          <button>+</button>
+        <div
+          className={`btn__container ${isLoading && "btn_container_isLoading"}`}
+        >
+          {isLoading ? (
+            <div className="loader"></div>
+          ) : (
+            <>
+              <button
+                onClick={handleClickDecrement}
+                className="decrement_button"
+              >
+                -
+              </button>
+              <p>{count}</p>
+              <button
+                onClick={handleClickIncrement}
+                className="increment_button"
+              >
+                +
+              </button>
+            </>
+          )}
         </div>
       </div>
-      <div>
-        <button>X</button>
+      <div className="close-icon__container">
+        <button>
+          <img src={require("../../../images/closeIcon.svg").default} />
+        </button>
       </div>
     </div>
   );
